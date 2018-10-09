c668d34d60e47d5828de4fb9973d67015259b8a9
imouto <xo.imouto@gmail.com>
2018-10-09 15:26:54 -0600

    Linked rectpack2D

--- src/pch.h
+++ src/pch.h
@@ -18,10 +18,12 @@ namespace fs = std::filesystem; // comfy alias
 #include "File.hpp"
 
 // Third Party
-#define GLEW_STATIC
-#include <gl/glew.h>
-#include <GLFW/glfw3.h>
-#include <zlib.h>
+
+#define GLEW_STATIC // glew
+#include <gl/glew.h> // glew
+#include <GLFW/glfw3.h> // GLFW
+#include <zlib.h> // zlib
+#include <finders_interface.h> // rectpack2D
 
 constexpr double GMLTrue = 1.0;
 constexpr double GMLFalse = 0.0;
