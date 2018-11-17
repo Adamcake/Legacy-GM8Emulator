2a59929c559f301b3e675bfce93e4f27b98ff754
imouto <xo.imouto@gmail.com>
2018-11-17 16:52:48 -0600

    hey you have push permission you know!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,100 @@
 #include <pch.h>
-#include <iostream>
-#include <chrono>
-#include <thread>
+
 #include "Game.hpp"
+#include <chrono>
+#include <iostream>
+#include <thread>
 
 #define CHECK_MEMORY_LEAKS 0
-#define OUTPUT_FRAME_TIME 1
+constexpr bool OUTPUT_FRAME_TIME = true;
 
 #if CHECK_MEMORY_LEAKS
 #define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>  
 #include <crtdbg.h>
+#include <stdlib.h>
 #endif
 
 int main(int argc, char** argv) {
 
 #if CHECK_MEMORY_LEAKS
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	std::chrono::high_resolution_clock::time_point t1, t2, t3;
+    // OUTPUT_FRAME_TIME (noop otherwise)
+    std::chrono::high_resolution_clock::time_point t1, t2, t3;
+    std::chrono::duration<double> time_span;
+    double se;
 
-#if OUTPUT_FRAME_TIME
-	t1 = std::chrono::high_resolution_clock::now();
-#endif
+    if constexpr (OUTPUT_FRAME_TIME) {
+        t1 = std::chrono::high_resolution_clock::now();
+    }
 
-	GameInit();
+    GameInit();
 
-	// This is just temp - you must place a game called "game.exe" in the project directory (or in the same directory as your built exe) to load it.
-	// This can easily be changed to load from anywhere when the project is done.
-	if (!GameLoad("game.exe")) {
-		// Load failed
-		GameTerminate();
-		return 2;
-	}
+    // This is just temp - you must place a game called "game.exe" in the project directory (or in the same directory as your built exe) to load it.
+    // This can easily be changed to load from anywhere when the project is done.
+    if (!GameLoad("game.exe")) {
+        // Load failed
+        GameTerminate();
+        return 2;
+    }
 
-#if OUTPUT_FRAME_TIME
-	t2 = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-	double se = time_span.count();
-	std::cout << "Successful load in " << se << " seconds" << std::endl;
-#endif
+    if constexpr (OUTPUT_FRAME_TIME) {
+        t2 = std::chrono::high_resolution_clock::now();
+        time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+        se = time_span.count();
+        std::cout << "Successful load in " << se << " seconds" << std::endl;
+    }
 
-	if (!GameStart()) {
-		// Starting game failed
+    if (!GameStart()) {
+        // Starting game failed
         const char* err;
         if (GameGetError(&err)) {
             std::cout << "RUNTIME ERROR: " << err << std::endl;
         }
 
-		GameTerminate();
-		return 3;
-	}
+        GameTerminate();
+        return 3;
+    }
 
-#if OUTPUT_FRAME_TIME
-	t3 = std::chrono::high_resolution_clock::now();
-	time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t3 - t1);
-	se = time_span.count();
-	std::cout << "Successful game start in " << se << " seconds" << std::endl;
-#endif
+    if constexpr (OUTPUT_FRAME_TIME) {
+        t3 = std::chrono::high_resolution_clock::now();
+        time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t3 - t1);
+        se = time_span.count();
+        std::cout << "Successful game start in " << se << " seconds" << std::endl;
+    }
 
-	unsigned int a = 0;
-	double totMus = 0;
-	while (true) {
-		t1 = std::chrono::high_resolution_clock::now();
-		if (!GameFrame()) {
+    unsigned int a = 0;
+    double totMus = 0;
+    while (true) {
+        t1 = std::chrono::high_resolution_clock::now();
+        if (!GameFrame()) {
             const char* err;
-            if(GameGetError(&err)) {
+            if (GameGetError(&err)) {
                 std::cout << "RUNTIME ERROR: " << err << std::endl;
             }
-			break;
-		}
-		
-#if OUTPUT_FRAME_TIME
-		t2 = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-		double mus = time_span.count() * 1000000.0;
-		std::cout << "Frame took " << (int)mus << " microseconds" << std::endl;
-#endif
+            break;
+        }
 
-		while (true) {
-			t2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-			double mus2 = time_span.count() * 1000000.0;
-			long long waitMus = (long long)((((double)1000000.0) / GameGetRoomSpeed()) - mus2);
-			if (waitMus <= 0) {
-				break;
-			}
-		}
-	}
+        if constexpr (OUTPUT_FRAME_TIME) {
+            t2 = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            double mus = time_span.count() * 1000000.0;
+            std::cout << "Frame took " << ( int )mus << " microseconds" << std::endl;
+        }
 
-	// Natural end of application
-	GameTerminate();
-	return 0;
+        while (true) {
+            t2 = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+            double mus2 = time_span.count() * 1000000.0;
+            long long waitMus = ( long long )(((( double )1000000.0) / GameGetRoomSpeed()) - mus2);
+            if (waitMus <= 0) {
+                break;
+            }
+        }
+    }
+
+    // Natural end of application
+    GameTerminate();
+    return 0;
 }
